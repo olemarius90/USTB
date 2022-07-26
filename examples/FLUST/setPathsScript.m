7119a4f40260fcd457d0a01ad43d8239bc86c8e2
jorgenav <jorgen.avdal@ntnu.no>
2022-07-26 21:38:40 +0200

    Added comments to setPathsScript

--- examples/FLUST/setPathsScript.m
+++ examples/FLUST/setPathsScript.m
@@ -1,10 +1,15 @@
+% for git repos, you may want to copy this file, use the copy in FLUST scripts, then let
+% git ignore the copy.
+
 lastwarn(''); % reset warning
 
 %% set paths here
 
 % PSF generators (Field II/MUST/other)
-addpath('PathToField_II')
-addpath('PathToMUST');
+addpath('PathToField'); % Path to Field II goes here
+% addpath('PathToMUST'); % Path to MUST if this is used
+
+% addpath('C:\Users\jorgenav\Software\field_IIpro\m_files');
 
 % FLUST folders
 addpath( genpath( pwd) ); % please run FLUST from its root folder
