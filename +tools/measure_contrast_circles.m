788703da077d8c463f5ff12bbee23ea03277a34d
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2022-11-30 13:22:27 +0100

    Clean up code

--- +tools/measure_contrast_circles.m
+++ +tools/measure_contrast_circles.m
@@ -19,14 +19,13 @@ idx_speckle = (points < (r)^2);
 
 
 %%
-b_data.plot([],['1=b-mode, 2=b-mode.*CF'],[],[],[],[],'m','dark')
-caxis([-60 0]);
-axi = gca;
-viscircles(axi,[xc_nonecho,zc_nonecho],r,'EdgeColor','r');
-viscircles(axi,[xc_echo,zc_echo],r,'EdgeColor','b');
-%b_data.frame_rate = 1;
-%b_data.save_as_gif(['Figures/vrak05_skagerrak/',filesep,'alternate_das_CF_zoom_indicated.gif'])
-
+if plot_flag
+    b_data.plot([],['Plot with regions indicated'],[],[],[],[],['m'],'dark')
+    caxis([-60 0]);
+    axi = gca;
+    viscircles(axi,[xc_nonecho,zc_nonecho],r,'EdgeColor','r');
+    viscircles(axi,[xc_echo,zc_echo],r,'EdgeColor','b');
+end
 %%
 img_signal = b_data.get_image('none');
 %%
@@ -37,60 +36,37 @@ for f = 1:b_data.N_frames
     mean_background = mean(abs(img_signal_current(idx_speckle(:))).^2)
     mean_ROI = mean(abs(img_signal_current(idx_cyst(:))).^2)
     
+    % Calculate CR
     CR(f) = 10*log10(mean_ROI/mean_background)
 
+    % Calculate CNR
     sigma_background = std( abs(img_signal_current(idx_speckle(:))).^2 );
     sigma_ROI = std( abs(img_signal_current(idx_cyst(:))).^2 );
     
     CNR(f) = abs(mean_ROI - mean_background) / sqrt(sigma_ROI^2 + sigma_background^2)
 
+    % Calculate gCNR
     x=linspace(min(db(abs(img_signal_current(:)))),max(db(abs(img_signal_current(:)))),100);
-
     [pdf_i]=hist(db(abs(img_signal_current(idx_cyst(:)))),x);
     [pdf_o]=hist(db(abs(img_signal_current(idx_speckle(:)))),x);
-
-%     mask_img_i = reshape(mask_i,size(img,1),size(img,2));
-%     figure();
-%     subplot(121);
-%     imagesc(img.*mask_i);
-%     subplot(122);
-%     imagesc(img.*mask_o);
-
-%% Plot probability density function
-
-    figure()
-    plot(x,pdf_i./sum(pdf_i),'r-', 'linewidth',2); hold on; grid on;
-    plot(x,pdf_o./sum(pdf_o),'b-', 'linewidth',2);
-    hh=area(x,min([pdf_i./sum(pdf_i); pdf_o./sum(pdf_o)]), 'LineStyle','none');
-    hh.FaceColor = [0.6 0.6 0.6];
-    xlabel('||s||');
-    ylabel('Probability');
-    legend('p_i','p_o','OVL');
-
-    set(gca,'FontSize', 14);
-
-    idx = find(min([pdf_i./sum(pdf_i); pdf_o./sum(pdf_o)])>0);
-    xlim([-200 0])
-
-
+    
     OVL =sum(min([pdf_i./sum(pdf_i); pdf_o./sum(pdf_o)]));
     MSR  = 1 - OVL/2;
     GCNR(f) = 1 - OVL;
-
     
-%     if plot_flag
-%         %%
-%         figure;
-%         subplot(211);
-%         imagesc(db(abs(img_signal_current)).*idx_cyst)
-%         caxis([-60 0]);
-%         colorbar
-%         subplot(212);
-%         imagesc(db(abs(img_signal_current)).*idx_speckle)
-%         caxis([-60 0]);
-%         colorbar;
-%     else
+    if plot_flag
+        %% Plot probability density function
+        figure()
+        plot(x,pdf_i./sum(pdf_i),'r-', 'linewidth',2); hold on; grid on;
+        plot(x,pdf_o./sum(pdf_o),'b-', 'linewidth',2);
+        hh=area(x,min([pdf_i./sum(pdf_i); pdf_o./sum(pdf_o)]), 'LineStyle','none');
+        hh.FaceColor = [0.6 0.6 0.6];
+        xlabel('||s||');
+        ylabel('Probability');
+        legend('p_i','p_o','OVL');
+        title(['gCNR from frame = ',num2str(f)])
+        set(gca,'FontSize', 14);
+    end
 end
-GCNR
 end
 
