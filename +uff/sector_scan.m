0a190ebc7cab3eea1734e9f17290bcd384087610
Stefano Fiorentini <stefano.fiorentini@ntnu.no>
2022-12-19 16:32:47 +0100

    changes to sector scan to add the behaviour of sector_scan_na

--- +uff/sector_scan.m
+++ +uff/sector_scan.m
@@ -1,7 +1,7 @@
 classdef sector_scan < uff.scan
-    %SECTOR_SCAN   UFF class to define a linear scan 
+    %SECTOR_SCAN   UFF class to define a sector scan 
     %   SECTOR_SCAN contains the position of the azimuth and depth axis
-    %   from a position apex.
+    %   from an origin. If more origins are present
     %
     %   Compulsory properties:
     %         x_axis           % Vector containing the x coordinates of the x - axis [m]
@@ -15,70 +15,82 @@ classdef sector_scan < uff.scan
     %
     %   See also UFF.SCAN, UFF.LINEAR_SCAN
 
-    %   authors: Alfonso Rodriguez-Molares (alfonso.r.molares@ntnu.no)
-    %   $Date: 2017/06/18 $
+    %   authors:    Alfonso Rodriguez-Molares (alfonso.r.molares@ntnu.no)
+    %               Anders E. Vrålstad (anders.e.vralstad@ntnu.no)
+    %               Stefano Fiorentini (stefano.fiorentini@ntnu.no)
+    %   $Date: 2022/12/19$
 
     properties  (Access = public)
         azimuth_axis                % Vector containing the azimuth coordinates of the azimuth axis [rad]
         depth_axis                  % Vector containing the distance coordinates of the distance axis [m]
-        apex                        % POINT class
+        origin                      % POINT class
     end
     
     properties  (Dependent)
-        N_azimuth_axis            % number of pixels in the x_axis
-        N_depth_axis              % number of pixels in the z_axis
-        depth_step                % the step size in m of the depth samples
-        reference_distance        % distance used for the calculation of the phase term      
+        N_azimuth_axis            % Number of pixels in azimuth_axis
+        N_depth_axis              % Number of pixels in depth_axis
+        N_origins                 % Number of scanline origins
+        depth_step                % Step size along the depth axis [m]
+        reference_distance        % Distance used for the calculation of the phase term [m]      
     end
     
     properties (Access = private)
-        theta                     % azimuth coordinates in radians
-        rho                       % depth coordinates in m
+        rho                       % Depth coordinates [m]
     end
     
     %% constructor -> uff constructor
     methods (Access = public)
         function h=sector_scan(varargin)
             h = h@uff.scan(varargin{:});
-            h.update_pixel_position();
-            if ~isa(h.apex, 'uff.point'), h.apex = uff.point(); end
         end
     end
     
     %% update pixel position
-    methods 
+    methods (Access = private)
         function h=update_pixel_position(h)
-            if isempty(h.azimuth_axis)||isempty(h.depth_axis)||isempty(h.apex) return; end
+            if isempty(h.azimuth_axis) || isempty(h.depth_axis) || isempty(h.origin) 
+                return
+            end
             
-            % defining the pixel mesh 
-            [h.theta h.rho]=meshgrid(h.azimuth_axis,h.depth_axis);
+            assert(h.N_origins == 1 || h.N_origins == h.N_azimuth_axis, ...
+                'Number of origins should be either one or equal to the number of scan lines.');
             
-            h.theta=h.theta(:);
-            h.rho=h.rho(:);
+            % Defining the pixel mesh
+            [rho, theta] = ndgrid(h.depth_axis, h.azimuth_axis); %#ok<*PROP>
+            
+            N_pixels = numel(rho);
+            
+            % Define origin struct
+            origin.x = [h.origin.x];
+            origin.y = [h.origin.y];
+            origin.z = [h.origin.z];
+            
+            % Storing in case the reference distance is needed
+            h.rho = rho(:);
             
             % position of the pixels
-            h.x=h.rho.*sin(h.theta)+h.apex.x;
-            h.y=0.*h.rho+h.apex.y;
-            h.z=h.rho.*cos(h.theta)+h.apex.z;
+            h.x=reshape(rho.*sin(theta)+origin.x, [N_pixels, 1]);
+            h.y=reshape(zeros(size(rho)), [N_pixels, 1]);
+            h.z=reshape(rho.*cos(theta)+origin.z, [N_pixels, 1]);
         end
     end
     
     %% Set methods
     methods
-        function h=set.azimuth_axis(h,in_azimuth_axis)
-            assert(size(in_azimuth_axis,2)==1, 'The input must be a column vector.')
-            h.azimuth_axis=in_azimuth_axis;
-            h=h.update_pixel_position();
+        function set.azimuth_axis(h,in_azimuth_axis)
+            assert(isvector(in_azimuth_axis)&&isfloat(in_azimuth_axis), 'The input must be a floating point vector.')
+            h.azimuth_axis=in_azimuth_axis(:);
+            h.update_pixel_position();
         end
-        function h=set.depth_axis(h,in_depth_axis)
-            assert(size(in_depth_axis,2)==1, 'The input vector must be a column vector.')
+        function set.depth_axis(h,in_depth_axis)
+            assert(isvector(in_depth_axis)&&isfloat(in_azimuth_axis), 'The input must be a floating point vector.')
             h.depth_axis=in_depth_axis;
-            h=h.update_pixel_position();
+            h.update_pixel_position();
         end
-        function h=set.apex(h,in_apex)
-            assert(isa(in_apex,'uff.point'), 'The input is not a SOURCE class. Check HELP SOURCE');
-            h.apex=in_apex;
-            h=h.update_pixel_position();
+        function set.origin(h,in_origin)
+            assert(isa(in_origin,'uff.point')&&isvector(in_origin), 'The input must be a vector of UFF.POINT objects.');
+            h.origin=in_origin;
+            h.update_pixel_position();
         end
     end
     
@@ -96,7 +108,9 @@ classdef sector_scan < uff.scan
         function value=get.reference_distance(h)
             value = h.rho;
         end
+        function value=get.N_origins(h)
+            value=numel(h.origin);
+        end
     end
-   
 end
 
